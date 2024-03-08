8891ef0402db4a7ee205d871bb015aaf7ccf177b
gumpy <asifbhshawon71@gmail.com>
2024-03-09 00:51:54 +0600

    9_3

---
+++ script.js
@@ -0,0 +1,6 @@
+const courses = [{
+    title: "Self-Defense Fundamentals",
+    short_description: "Learn the basics of self-defense and how to protect yourself in dangerous situations.",
+    price: 2000,
+    images: ["self-defense.jpg"],
+}] // array of courses
