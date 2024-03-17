f9575fc03d6aceaa61c2b5fcb50cbfc1df013900
gumpy <asifbhshawon71@gmail.com>
2024-03-17 22:21:12 +0600

    ___

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 <body class="main-body">
   
  <?php include 'navbar.html'; ?>
- 
   <!-- Hero section start -->
   <div class="hero-sec">
     <!-- Hero 3 - Bootstrap Brain Component -->
@@ -149,76 +148,9 @@
       </div>
     </div>
   </section>
-
-
-
   <!-- Course section end -->
 
-  <!-- Footer section start -->
-  <div class="footer-sec-1">
-    <div class="container">
-      <footer class="py-5">
-        <div class="row">
-          <div class="col mb-3">
-            <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-              <img src="./components/footer-logo.png" alt="" class="footer-logo">
-            </a>
-            <p class="h2">LearnToProtect</p>
-          </div>
-
-          <div class="col-6 col-md-2 mb-3">
-            <h5>LINKS</h5>
-            <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="p-0 nav-link-text">Home</a></li>
-              <li class="nav-item mb-2"><a href="#" class="p-0 nav-link-text">About us</a></li>
-              <li class="nav-item mb-2"><a href="#" class="p-0 nav-link-text">Pricing</a></li>
-              <li class="nav-item mb-2"><a href="#" class="p-0 nav-link-text">FAQs</a></li>
-              <li class="nav-item mb-2"><a href="#" class="p-0 nav-link-text">About</a></li>
-            </ul>
-          </div>
-
-          <!-- <div class="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-          </ul>
-        </div> -->
-
-          <div class="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Have some Questions in your mind?</h5>
-              <p>We're here to listen you...</p>
-              <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label for="newsletter1" class="visually-hidden">Email address</label>
-                <!-- <input id="newsletter1" type="text" class="form-control" placeholder="Email address"> -->
-                <button class="btn btn-dark" type="button">Contact Us</button>
-              </div>
-            </form>
-          </div>
-        </div>
-    </div>
-    <div class="footer-sec-2">
-      <div class="container d-flex flex-column flex-sm-row justify-content-between">
-        <p>© 2024 LearnToProtect | All rights reserved.</p>
-        <ul class="list-unstyled d-flex">
-          <li class="ms-3"><a class="link-dark social-link-color" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#twitter"></use>
-              </svg></a></li>
-          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#instagram"></use>
-              </svg></a></li>
-          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#facebook"></use>
-              </svg></a></li>
-        </ul>
-      </div>
-    </div>
-    </footer>
-  </div>
+  <?php include 'footer.html' ?>
 
   <!-- JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
